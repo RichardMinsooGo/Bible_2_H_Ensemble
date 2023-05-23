c216d5f57875b9fe4b2b2fd4de904fe9b41b7b1b
Richard Minsoo Go <kauntach@naver.com>
2023-05-23 12:15:23 +0900

    Update and rename 06_23_bagging_TF2_MNIST_subclassing_multi_model.py to A2_bagging_TF2_MNIST_subclassing_multi_model.py

--- A2_bagging_TF2_MNIST_subclassing_multi_model.py
+++ A2_bagging_TF2_MNIST_subclassing_multi_model.py
@@ -1,20 +1,15 @@
+'''
+Data Engineering
+'''
+
+'''
+D1. Import Libraries for Data Engineering
+'''
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout
-from tensorflow.keras import Model, Sequential
 
-from tensorflow.keras.utils import to_categorical
-import matplotlib.pyplot as plt
-from tensorflow.keras import Input
-
-import numpy as np
-
-# Hyper-parameters 
-input_size = 784 # 28x28
-hidden_size = 256 
-num_classes = 10
-EPOCHS = 5
-batch_size = 100
-learning_rate = 0.001
+'''
+D2. Load MNIST data / Only for Toy Project
+'''
 
 # print(tf.__version__)
 ## MNIST Dataset #########################################################
@@ -26,32 +21,28 @@ mnist = tf.keras.datasets.mnist
 # mnist = tf.keras.datasets.fashion_mnist
 # class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 ##########################################################################
-(X_train, Y_train), (X_test, Y_test) = mnist.load_data()    
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
-# Change data type as float. If it is unt type, it might cause error 
+# Change data type as float. If it is int type, it might cause error
+'''
+D3. Data Preprocessing
+'''
+# Normalizing
 X_train, X_test = X_train / 255.0, X_test / 255.0
 
 print(Y_train[0:10])
 print(X_train.shape)
 
-# in the case of Keras or TF2, type shall be [image_size, image_size, 1]
-# if it is RGB type, type shall be [image_size, image_size, 3]
-# For MNIST or Fashion MNIST, it need to reshape
-X_train_new_axis = np.expand_dims(X_train, axis=-1)
-X_test_new_axis = np.expand_dims(X_test, axis=-1)
-    
-Y_train = to_categorical(Y_train, 10)
-Y_test = to_categorical(Y_test, 10)    
-    
-# 입력된 buffer_size만큼 data를 채우고 무작위로 sampling하여 새로운 data로 바꿉니다.
-# 완벽한 셔플링을 위해서는 데이터 세트의 전체 크기보다 크거나 같은 버퍼 크기가 필요합니다.
-# 만약 작은 데이터수보다 작은 buffer_size를 사용할경우,
-# 처음에 설정된 buffer_size만큼의 data안에서 임의의 셔플링이 발생합니다.
-shuffle_size = 100000
+# One-Hot Encoding
+from tensorflow.keras.utils import to_categorical
 
-train_ds = tf.data.Dataset.from_tensor_slices(
-    (X_train_new_axis, Y_train)).shuffle(shuffle_size).batch(batch_size)
-test_ds  = tf.data.Dataset.from_tensor_slices((X_test_new_axis, Y_test)).batch(batch_size)
+Y_train = to_categorical(Y_train, 10)
+Y_test = to_categorical(Y_test, 10)
+
+'''
+D4. EDA(? / Exploratory data analysis)
+'''
+import matplotlib.pyplot as plt
 
 # plot first few images
 for i in range(9):
@@ -64,6 +55,55 @@ for i in range(9):
     
 # show the figure
 plt.show()
+
+'''
+D5. Build dataset
+'''
+batch_size = 100
+# in the case of Keras or TF2, type shall be [image_size, image_size, 1]
+# if it is RGB type, type shall be [image_size, image_size, 3]
+# For MNIST or Fashion MNIST, it need to reshape
+
+import numpy as np
+X_train_new_axis = np.expand_dims(X_train, axis=-1)
+X_test_new_axis = np.expand_dims(X_test, axis=-1)
+    
+# It fills data as much as the input buffer_size and randomly samples and replaces it with new data.
+# Perfect shuffling requires a buffer size greater than or equal to the total size of the data set.
+# If you use a buffer_size smaller than the small number of data, 
+# random shuffling occurs within the data as much as the initially set buffer_size.
+
+shuffle_size = 100000
+
+train_ds = tf.data.Dataset.from_tensor_slices(
+    (X_train_new_axis, Y_train)).shuffle(shuffle_size).batch(batch_size)
+test_ds = tf.data.Dataset.from_tensor_slices(
+    (X_test_new_axis, Y_test)).batch(batch_size)
+
+'''
+Model Engineering
+'''
+
+'''
+M1. Import Libraries for Model Engineering
+'''
+
+from tensorflow.keras import Model, Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten, Conv2D, MaxPool2D, Dropout
+from tensorflow.keras import optimizers
+from tensorflow.keras import Input
+import numpy as np
+
+'''
+M2. Set Hyperparameters
+'''
+
+hidden_size = 256
+output_dim = 10      # output layer dimensionality = num_classes
+EPOCHS = 10
+learning_rate = 0.001
+
 
 class MNISTModel(tf.keras.Model):
     def __init__(self):
@@ -140,19 +180,38 @@ model_3 = MNISTModel_3()
 models = [model_1, model_2, model_3]
 num_models = len(models)
 
+'''
+M4. Optimizer
+'''
+
 optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
-
+'''
+M5. Define Loss Function
+'''
+# Forward Pass included
 def loss_fn(model, images, labels):
     logits = model(images, training=True)
     loss = tf.reduce_mean(tf.keras.losses.categorical_crossentropy(
         y_pred=logits, y_true=labels, from_logits=True))    
     return loss   
 
+'''
+M6. Define train loop
+'''
+
 def grad(model, images, labels):
     with tf.GradientTape() as tape:
         loss = loss_fn(model, images, labels)
     return tape.gradient(loss, model.variables)
+
+def train_step(model, images, labels):
+    gradients = grad(model, images, labels)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+'''
+M7. Metrics - Accuracy
+'''
 
 def evaluate(models, images, labels):
     predictions = np.zeros_like(labels)
@@ -163,18 +222,19 @@ def evaluate(models, images, labels):
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     return accuracy
 
+# checkpoint was not used in this implementation
 checkpoints = []
 for m in range(num_models):
     checkpoints.append(tf.train.Checkpoint(cnn=models[m]))
 
-def train_step(model, images, labels):
-    gradients = grad(model, images, labels)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+'''
+M8. Define Episode / each step process
+'''
 
 from tqdm import tqdm, tqdm_notebook, trange
 
 for epoch in range(EPOCHS):
-
+    
     with tqdm_notebook(total=len(train_ds), desc=f"Train Epoch {epoch+1}") as pbar:    
         train_losses = []
         train_accuracies = []
@@ -192,19 +252,22 @@ for epoch in range(EPOCHS):
             
             pbar.update(1)
             pbar.set_postfix_str(f"Loss: {loss_val:.4f} ({np.mean(train_losses):.4f}) Acc: {acc:.3f} ({np.mean(train_accuracies):.3f})")
-
-    with tqdm_notebook(total=len(test_ds), desc=f"Test_ Epoch {epoch+1}") as pbar:    
-        test_losses = []
-        test_accuracies = []
-        for test_images, test_labels in test_ds:
-            for model in models:
-                loss_val = loss_fn(model, test_images, test_labels)
-
-                test_losses.append(loss_val / num_models)
-            acc = 100 * evaluate(models, test_images, test_labels)
             
-            test_accuracies.append(acc)
+'''
+M9. Model evaluation
+'''
+with tqdm_notebook(total=len(test_ds), desc=f"Test_ Epoch {epoch+1}") as pbar:    
+    test_losses = []
+    test_accuracies = []
+    for test_images, test_labels in test_ds:
+        for model in models:
+            loss_val = loss_fn(model, test_images, test_labels)
 
-            pbar.update(1)
-            pbar.set_postfix_str(f"Loss: {loss_val:.4f} ({np.mean(test_losses):.4f}) Acc: {acc:.3f} ({np.mean(test_accuracies):.3f})")
-            
+            test_losses.append(loss_val / num_models)
+        acc = 100 * evaluate(models, test_images, test_labels)
+
+        test_accuracies.append(acc)
+
+        pbar.update(1)
+        pbar.set_postfix_str(f"Loss: {loss_val:.4f} ({np.mean(test_losses):.4f}) Acc: {acc:.3f} ({np.mean(test_accuracies):.3f})")
+
